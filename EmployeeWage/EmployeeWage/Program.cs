3797987e3f3888f5394961191a02909bb9c2fc67
Princekotadiya007 <princuuu77@gmail.com>
2023-04-04 17:57:33 +0530

    [prince] emplopyee present or absent

---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Attandance attandance = new Attandance();
+            attandance.Attendance();
+        }
+    }
+}
